0d5a8106da474c4570e3cf313b4b19badc225f33
bernas-ernesto <thonbernas1134@gmail.com>
2025-03-24 15:46:00 +0800

    Add logic of grand total to the repository.

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ def get_orders(): # TODO BUENACIFRA
 def get_customer_name_and_senior_id(): # TODO ROLDAN
 
 def get_grand_total(): # TODO BERNAS
+    grand_total = sum(order["Total"] for order in get_orders)
+    if if_senior == "y":
+        senior_id = input("Senior Citizen ID: ")
+    grand_total /= 10
 
 def display_orders_customer_name_senior_id_grand_total(): # TODO TERO
