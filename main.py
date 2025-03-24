32688f0ba09a0d2623db041cabe23dbed762068e
buenacifra-abrianne <abriannebuenacifra@gmail.com>
2025-03-24 12:12:41 +0800

    Add initial file and distribution of tasks

---
+++ main.py
@@ -0,0 +1,7 @@
+def get_orders(): # TODO BUENACIFRA
+
+def get_customer_name_and_senior_id(): # TODO ROLDAN
+
+def get_grand_total(): # TODO BERNAS
+
+def display_orders_customer_name_senior_id_grand_total(): # TODO TERO
