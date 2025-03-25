e0d91cd498556ba86eacabd665f2d52d714d6920
roldan-gianrafael <gianrafaelroldan13@gmail.com>
2025-03-25 16:32:25 +0800

    add main and update customer name and senior id

--- main.py
+++ main.py
@@ -21,32 +21,33 @@ def get_orders(): # TODO BUENACIFRA
 
 def get_customer_name_and_senior_id(): # TODO ROLDAN
     customer_name = input("Enter customer name: ")
-    if_senior = input("Is the customer a senior citizen? (y/n) leave it blank id not applicable: ").lower()
+    if_senior = input("Is the customer a senior citizen? (y if yes Leave blank if not senior citizen) ").lower()
     senior_id = ""
-    return customer_name, if_senior
-
-def get_grand_total(): # TODO BERNAS
-    grand_total = sum(order["Total"] for order in get_orders)
     if if_senior == "y":
-        senior_id = input("Senior Citizen ID: ")
-    grand_total /= 10
+        senior_id = input("Enter senior id")
+    return customer_name, if_senior, senior_id
 
-def display_orders_customer_name_senior_id_grand_total():  # TODO TERO
-    customer_name, if_senior = get_customer_name_and_senior_id()
-    orders = get_orders()
+def get_grand_total(orders, if_senior): # TODO BERNAS
     grand_total = sum(order["Total"] for order in orders)
-    
     if if_senior == "y":
-        senior_id = input("Senior Citizen ID: ")
         discount = grand_total * 0.10
         grand_total -= discount
+    return grand_total
+
+def display_orders_customer_name_senior_id_grand_total(): # TODO TERO
+    orders = get_orders()
+    customer_name, if_senior, senior_id = get_customer_name_and_senior_id()
+    grand_total = get_grand_total(orders, if_senior)
     
+    print("\nItems:")
+    for order in orders:
+        print(f"Product Name: {order['Product Name']}, Price: {order['Price']}, Quantity: {order['Quantity']}, Total: {order['Total']}")
     print("\nCustomer Name:", customer_name)
     if if_senior == "y":
-        print("Senior Citizen ID:", senior_id)
-    print("\nOrders:")
-    for order in orders:
-        print(order)
-    print("\nGrand Total:", grand_total)
+        print("Senior id no.:", senior_id)
+    print("Grand total:", grand_total)
 
+def main():
+    display_orders_customer_name_senior_id_grand_total()
 
+main()
